2bb03bcd1bec0561df9a2193da979ecbcd1987db
Benjamin <benjamin.trabant@my.jcu.edu.au>
2015-05-28 21:18:14 +1000

    started contact page

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	include_once('include/header.php');
 ?>
 
-<h1> Home </h1> 
+<h1> Homer </h1> 
 <body>
     <p>Based in the beautiful Townsville, North Queensland, the Music Centre presents concerts and workshops throughout the year. The Musica Centre boasts  a diverse range of genres including classical, jazz, folk, blues, world and contemporary music, featuring touring artists and locally-based professional and emerging artists.</p>
 
