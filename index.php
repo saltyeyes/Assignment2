489bd9309fcf44c62a02b2cdb0d9dc155973f869
insearchoftheshoe <jessica.simpson1@my.jcu.edu.au>
2015-05-28 19:16:00 +1000

    lets see how this looks
    yeah

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
-<?php
 
+<?php
 	// set page title variable here
 	$page_title = "Home";
-
 	include_once('include/header.php');
-
 ?>
 
+<h1> Home </h1> 
+<body>
+    <h2>Based in the beautiful Townsville, North Queensland, the Music Centre presents concerts and workshops throughout the year. The Musica Centre boasts  a diverse range of genres including classical, jazz, folk, blues, world and contemporary music, featuring touring artists and locally-based professional and emerging artists.</h2>
 
-
+</body>
 
 
 <?php
-
 	include_once('include/footer.php');
-
 ?>
